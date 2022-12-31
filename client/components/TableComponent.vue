141e235d6f96408cc6ad0527d1ce64a4b1973a90
ibkhall <ibrahimkhall212@gmail.com>
2022-12-31 17:44:13 +0100

    refactor: remove unsed props from TableComponent

--- client/components/TableComponent.vue
+++ client/components/TableComponent.vue
@@ -2,7 +2,7 @@
 import { Model } from '../models/Model';
 import { mdiChevronUp , mdiChevronDown, mdiBookOpenPageVariantOutline  } from '@mdi/js';
 import { SortDirection } from '@tailflow/laravel-orion/lib/drivers/default/enums/sortDirection';
-
+const {$emitter} = useNuxtApp()
 enum Direction {
   Asc = 'asc',
   Desc = 'desc',
@@ -10,21 +10,21 @@ enum Direction {
 }
 
 
-interface Header {title: string, value: string, class: string, sortable?: boolean, sort?: Direction}
+interface Header {title: string, value: string, class: string, sortable?: boolean, sort?: Direction, width?: string}
 
 const props = defineProps<{
     headers: Array<Header>,
     model: Model,
-    deletable: boolean,
-    editable: boolean
+    relations?: Array<string>,
 }>()
+const bus = ref(null)
 const data = ref<Array<Model>>([])
 const page = ref(0)
 const perPage = ref(0)
 const paginationLength = ref(0)
 const search = ref('')
 const loading = ref(true)
-data.value = await props.model.$query().get()
+data.value = await props.model.$query().with(props.relations ?? []).get()
 props.headers.forEach(el => {
     if(el.sortable) {
         el.sort = Direction.None
@@ -35,6 +35,12 @@ page.value = data.value[0]?.$response?.data.meta.page
 perPage.value = data.value[0]?.$response?.data.meta.per_page
 paginationLength.value = data.value[0]?.$response?.data.meta.last_page
 loading.value = false
+
+$emitter.on('onDelete', async () => {
+        data.value = await props.model.$query().with(props.relations ?? []).lookFor(search.value).search(perPage.value, page.value)
+    })
+
+
 const onPageChange = async (p: number) => {
     loading.value = true
     data.value = await props.model.$query().lookFor(search.value).search(perPage.value,p)
@@ -81,16 +87,16 @@ const toggleDirection = async (item: Header) => {
 </script>
 <template>
 <div>
-    <v-row justify="end">
+    <v-row justify="start">
         <v-col md="2">
-            <v-select @update:model-value="onPerPage" v-model="perPage" bg-color="grey-lighten-4" :prepend-inner-icon="mdiBookOpenPageVariantOutline" variant="solo" :items="[10,15,30]" density="compact" class="mb-1"  hide-details></v-select>
+            <v-select @update:model-value="onPerPage" v-model="perPage" bg-color="blue-grey-lighten-5" :prepend-inner-icon="mdiBookOpenPageVariantOutline" variant="outlined" :items="[10,15,30]" density="compact" class="mb-1"  hide-details></v-select>
 
         </v-col>
         <v-col md="3">
-            <v-text-field bg-color="grey-lighten-4" @update:model-value="onSearch" variant="solo" v-model="search" density="compact" class="mb-1" clearable hide-details placeholder="Search..." color="grey" type="search"></v-text-field>
+            <v-text-field bg-color="blue-grey-lighten-5" @update:model-value="onSearch" variant="outlined" v-model="search" density="compact" class="mb-1" clearable hide-details placeholder="Recherche..." color="grey" type="search"></v-text-field>
         </v-col>
     </v-row>
-    <v-table>
+    <v-table hover class="border border-md rounded d-block border-primary rounded-t-lg">
         <tr>
             <td :colspan="headers.length">
                 <v-progress-linear height="5" :active="loading" color="primary" indeterminate></v-progress-linear></td>
@@ -100,12 +106,12 @@ const toggleDirection = async (item: Header) => {
                 <div class="text-center cursor-pointer">Aucune donnée</div>
             </td>
         </tr>
-    <thead class="elevation-5">
+    <thead class="elevation-1">
         <tr>
             <template v-for="item,i in headers">
-                <th class="bg-pink" :class="{'rounded-ts-lg': i===0, 'rounded-te-lg': i===headers.length-1}">
+                <th class="bg-blue-grey-lighten-4" :class="{'rounded-ts-lg': i===0, 'rounded-te-lg': i===headers.length-1, [item.class]: true}">
                    <template v-if="item.sortable">
-                    <v-btn  v-ripple @click="toggleDirection(item)" variant="text" color="blue-grey-lighten-3">
+                    <v-btn  v-ripple @click="toggleDirection(item)" variant="text" color="primary">
                     {{ item.title }}
                     <template v-slot:append>
                         <div>
@@ -121,19 +127,19 @@ const toggleDirection = async (item: Header) => {
 
                     </v-btn>
                    </template>
-                    <span class="text-blue-grey-lighten-3" v-else>{{ item.title }}</span>
+                    <span class="text-primary text-uppercase" v-else>{{ item.title }}</span>
                 </th>
             </template>
         </tr>
     </thead>
     <tbody>
-        <template v-for="item in data">
+        <template v-for="item in data[0]?.$response?.data.data">
             <tr>
                 <template v-for="head in headers">
-                    <td class="border">
+                    <td :class="{[head.class]: true}" :width="head.width">
 
-                        <slot v-if="!$slots[head.value]" :name="head.value">{{ item.$attributes[head.value] }}</slot>
-                        <slot v-if="$slots[head.value]" :name="head.value" v-bind="item.$attributes"></slot>
+                        <slot v-if="!$slots[head.value]" :name="head.value">{{ item[head.value] }}</slot>
+                        <slot v-if="$slots[head.value]" :name="head.value" v-bind="{item: item, data: data[0].$response?.data.data}"></slot>
                     </td>
                 </template>
             </tr>
@@ -147,7 +153,7 @@ const toggleDirection = async (item: Header) => {
                 @update:model-value="onPageChange"
                 border
                 density="comfortable"
-                active-color="pink-accent-3"
+                active-color="primary"
                 v-model="page"
                 :length="paginationLength"
                 :total-visible="7"
